e44b471f72c6d65967fd7c97839fa17bc142cd29
Bruno Rijsman <brunorijsman@gmail.com>
2013-08-16 15:33:15 +0200

    Problem 88

--- euler/__main__.py
+++ euler/__main__.py
@@ -1,6 +1,9 @@
 import problem001
 import problem002
+import problem088
 import unittest
+
+only_fast_tests = False
 
 class EulerTest(unittest.TestCase):
 
@@ -9,6 +12,12 @@ class EulerTest(unittest.TestCase):
 
     def test_problem002(self):
         self.assertEqual(problem002.solve(), 4613732)
+
+    def test_problem088(self):
+        self.assertEqual(problem088.solve_max_size(6), 30)
+        self.assertEqual(problem088.solve_max_size(12), 61)
+        if not only_fast_tests:
+            self.assertEqual(problem088.solve(), 7587457)
 
 unittest.main()
 
